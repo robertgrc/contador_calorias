36e9f6facf59ac4b42211bf2147eac81d8dfbb1f
robertgrc <robert.rodriguez.rgrc@gmail.com>
2024-04-30 10:00:32 -0400

    validacion para mostrar el mensaje de guardado en el formulario

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -20,7 +20,11 @@ export default function Form() {
         [e.target.id]: isNumberField? +e.target.value: e.target.value
     })
  }
- 
+ const isValidActivity = () =>{
+    const {name, calories} = activity
+    console.log(name.trim() !== '' && calories > 0)
+    return name.trim() !== '' && calories > 0
+ }
 
   return (
     <form className="space-y-5 bg-white shadow p-10 rounded-lg">
@@ -69,8 +73,10 @@ export default function Form() {
 
         <input 
             type="submit"
-            className="bg-gray-800 hover:bg-gray-900 w-full p-2 font-bold uppercase text-white cursor-pointer" 
+            className="bg-gray-800 hover:bg-gray-900 w-full p-2 font-bold uppercase text-white cursor-pointer
+                       disabled:opacity-10" 
             value="Guardar Comida o Guardar Ejercicio"
+            disabled={!isValidActivity()}
         />
 
     </form>
