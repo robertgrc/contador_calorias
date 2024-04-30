b1efabdd7c772484c655c8ef4e9b8e11af8a15ed
robertgrc <robert.rodriguez.rgrc@gmail.com>
2024-04-29 20:43:47 -0400

    Creando los Contenedores principales

---
+++ src/components/Form.tsx
@@ -0,0 +1,52 @@
+import { categories } from "../data/categories"
+
+
+export default function Form() {
+  return (
+    <form className="space-y-5 bg-white shadow p-10 rounded-lg">
+        <div className="grid grid-cols-1 gap-3">
+            <label htmlFor="category" className="font-bold">Categoria:</label>
+            <select 
+                className="border border-slate-300 p-2 rounded-lg w-full bg-white"
+                id="category"
+            >
+              {categories.map(category=>(
+                <option 
+                    key={category.id}
+                    value={category.id}
+                >
+                    {category.name}
+                </option>
+              ))}
+            </select>
+        </div>
+
+        <div className="grid grid-cols-1 gap-3">
+            <label htmlFor="activity" className="font-bold">Actividad:</label>
+            <input 
+                id="activity"
+                type="text"
+                className="border border-slate-300 p-2 rounded-lg"
+                placeholder="Ej. Comida, Jugo de Naranja, Ensalada, Ejercicio, pesas, bicicleta"
+            />
+        </div>
+
+        <div className="grid grid-cols-1 gap-3">
+            <label htmlFor="calories" className="font-bold">Calorias:</label>
+            <input 
+                id="calories"
+                type="number"
+                className="border border-slate-300 p-2 rounded-lg"
+                placeholder="calorias ejem. 700 o 1400"
+            />
+        </div>
+
+        <input 
+            type="submit"
+            className="bg-gray-800 hover:bg-gray-900 w-full p-2 font-bold uppercase text-white cursor-pointer" 
+            value="Guardar Comida o Guardar Ejercicio"
+        />
+
+    </form>
+  )
+}
