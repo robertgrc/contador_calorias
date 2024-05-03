b2b005b92db971854f00f57a13dae60d1233bca8
robertgrc <robert.rodriguez.rgrc@gmail.com>
2024-05-03 14:51:36 -0400

    mostrando mensaje cuando no hay actividades

--- src/components/ActivityList.tsx
+++ src/components/ActivityList.tsx
@@ -16,12 +16,15 @@ export default function ActivityList({activities, dispatch}:ActivityListProps) {
     (category:Activity['category'])=>categories.map( cat=> cat.id ===category ? cat.name: '')
   , [activities])
 
+  const isEmptyActivities = useMemo(()=>activities.length===0, [activities])
+
   return (
     <>
         <h2 className='text-4xl font-bold text-slate-600 text-center'>
             Comida y Actividades
         </h2>
-        { activities.map( activity =>(
+        {isEmptyActivities? <p className='text-center'>No hay actividades aún..</p>:
+         activities.map( activity =>(
             <div 
                 key={activity.id}
                 className='px-5 py-10 bg-white mt-5 flex justify-between'
