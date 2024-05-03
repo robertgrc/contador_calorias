a1c873f430fc22988cb7df63bed32a2b2ac1c6a0
robertgrc <robert.rodriguez.rgrc@gmail.com>
2024-05-03 16:56:34 -0400

    Mostrando el diferencial de las Calorias

--- src/components/CalorieTracker.tsx
+++ src/components/CalorieTracker.tsx
@@ -1,5 +1,6 @@
-import { useMemo } from 'react'
+import { act, useMemo } from 'react'
 import { Activity } from '../types'
+import CalorieDisplay from './CalorieDisplay'
 
 type CalorieTrackerProps ={
     activities: Activity[]
@@ -11,20 +12,26 @@ export default function CalorieTracker({activities}:CalorieTrackerProps) {
 const caloriesConsumed = useMemo(()=>activities.reduce((total,activity)=>activity.category===1? total+activity.calories : total, 0),[activities])
 const caloriesBurned = useMemo(()=>activities.reduce((total,activity)=>activity.category===2? total+activity.calories : total, 0),[activities])
 
+const netCalories = useMemo(()=> caloriesConsumed-caloriesBurned,[activities])
+
   return (
     <>
         <h2 className='text-4xl font-black text-white text-center'>
             Resumen de calorias
         </h2>
         <div className='flex flex-col items-center md:flex-row md:justify-between gap-5 mt-10'>
-            <p className='text-white font-bold rounded-full grid grid-cols-1 gap-3 text-center'>
-                <span className='font-black text-6xl text-orange'>{caloriesConsumed}</span>
-                Consumidas
-            </p>
-            <p className='text-white font-bold rounded-full grid grid-cols-1 gap-3 text-center'>
-                <span className='font-black text-6xl text-orange'>{caloriesBurned}</span>
-                Quemadas
-            </p>
+            <CalorieDisplay 
+                calories={caloriesConsumed}
+                text={'Consumidas'}
+            />
+            <CalorieDisplay 
+                calories={caloriesBurned}
+                text={'Quemadas'}
+            />
+            <CalorieDisplay 
+                calories={netCalories}
+                text={'Diferencia'}
+            />
         </div>
     </>
   )
