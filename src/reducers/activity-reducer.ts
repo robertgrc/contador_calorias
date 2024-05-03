37ab2e6b94a0f620b6e95fa931a257e320c1850e
robertgrc <robert.rodriguez.rgrc@gmail.com>
2024-05-03 13:32:16 -0400

    Creando accion para eliminar actividades

--- src/reducers/activity-reducer.ts
+++ src/reducers/activity-reducer.ts
@@ -2,7 +2,9 @@ import { Activity } from "../types"
 
 export type ActivityActions =
 { type: 'save-activity', payload: { newActivity : Activity } } |
-{ type: 'set-activeId', payload: { id : Activity['id'] } }
+{ type: 'set-activeId', payload: { id : Activity['id'] } } |
+{ type: 'delete-activity', payload: { id : Activity['id'] } } 
+
 
 export type ActivityState={
     activities : Activity[]
@@ -43,5 +45,13 @@ export const activityReducer = (
             activeId: action.payload.id
         }
     }
+
+    if(action.type==='delete-activity'){
+        return{
+            ...state,
+            activities: state.activities.filter( activity => activity.id !== action.payload.id )
+        }
+    }
+
     return state
 }
