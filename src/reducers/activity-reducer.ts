b289895eb46486ffd2ac6f48ae82173e608aa8a3
robertgrc <robert.rodriguez.rgrc@gmail.com>
2024-05-03 13:25:50 -0400

    Actualizando las actividades

--- src/reducers/activity-reducer.ts
+++ src/reducers/activity-reducer.ts
@@ -22,12 +22,19 @@ export const activityReducer = (
   ) => {
 
     if(action.type === 'save-activity'){
-        //Este codigo maneja la logica para actualizar el state
-        // console.log(action.payload.newActivity)
+
+        let updatedActivities : Activity[] = []
+
+        if(state.activeId){
+            updatedActivities = state.activities.map(activity => activity.id ===state.activeId? action.payload.newActivity: activity)
+        } else {
+            updatedActivities = [...state.activities, action.payload.newActivity]
+        }
 
         return{
             ...state,
-            activities:[...state.activities, action.payload.newActivity]
+            activities: updatedActivities,
+            activeId: ''
         }
     }
     if(action.type==='set-activeId'){
